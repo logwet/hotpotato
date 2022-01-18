1df543777bec511c89ab161d5e93b62132e26bd0
logwet <logwet@users.noreply.github.com>
2022-01-18 14:31:54 +1100

    Remove unused Shadow

--- src/main/java/me/logwet/hotpotato/mixin/common/PlayerMixin.java
+++ src/main/java/me/logwet/hotpotato/mixin/common/PlayerMixin.java
@@ -3,7 +3,6 @@ package me.logwet.hotpotato.mixin.common;
 import com.google.common.collect.ImmutableSet;
 import java.util.Set;
 import me.logwet.hotpotato.HotPotato;
-import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -12,7 +11,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -26,9 +24,6 @@ public abstract class PlayerMixin extends LivingEntity {
     protected PlayerMixin(EntityType<? extends LivingEntity> entityType, Level level) {
         super(entityType, level);
     }
-
-    @Shadow
-    public abstract boolean hurt(DamageSource source, float amount);
 
     @Inject(method = "tick", at = @At("TAIL"))
     private void checkStatus(CallbackInfo ci) {
