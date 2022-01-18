503737e841512c11d17d00edb4721c548d52cce0
logwet <logwet@users.noreply.github.com>
2022-01-18 17:47:11 +1100

    Create place build

--- src/main/java/me/logwet/hotpotato/mixin/common/block/BlockMixin.java
+++ src/main/java/me/logwet/hotpotato/mixin/common/block/BlockMixin.java
@@ -2,11 +2,11 @@ package me.logwet.hotpotato.mixin.common.block;
 
 import me.logwet.hotpotato.PlayerPatch;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,15 +15,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Block.class)
 public abstract class BlockMixin {
-    @Inject(method = "playerDestroy", at = @At("TAIL"))
+    @Inject(method = "setPlacedBy", at = @At("TAIL"))
     private void resetTimeTracker(
             Level level,
-            Player player,
             BlockPos pos,
             BlockState state,
-            BlockEntity blockEntity,
-            ItemStack tool,
+            LivingEntity placer,
+            ItemStack stack,
             CallbackInfo ci) {
-        ((PlayerPatch) player).hotpotato$resetTimeTracker();
+        if (placer instanceof Player) {
+            ((PlayerPatch) placer).hotpotato$resetTimeTracker();
+        }
     }
 }
