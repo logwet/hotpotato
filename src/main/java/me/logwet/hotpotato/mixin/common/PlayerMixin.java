fbb58de813358c61916c01c3e6d0d1857034c06b
logwet <logwet@users.noreply.github.com>
2022-01-19 15:47:04 +1100

    Create floor build

--- src/main/java/me/logwet/hotpotato/mixin/common/PlayerMixin.java
+++ src/main/java/me/logwet/hotpotato/mixin/common/PlayerMixin.java
@@ -1,13 +1,11 @@
 package me.logwet.hotpotato.mixin.common;
 
-import java.util.Objects;
 import me.logwet.hotpotato.HotPotato;
 import me.logwet.hotpotato.PlayerPatch;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,7 +14,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Player.class)
 public abstract class PlayerMixin extends LivingEntity implements PlayerPatch {
-    @Unique private Vec3 lastPos;
     @Unique private int timeTracker = 0;
 
     protected PlayerMixin(EntityType<? extends LivingEntity> entityType, Level level) {
@@ -30,12 +27,10 @@ public abstract class PlayerMixin extends LivingEntity implements PlayerPatch {
 
     @Inject(method = "tick", at = @At("TAIL"))
     private void checkStatus(CallbackInfo ci) {
-        Vec3 position = this.position();
-        if (Objects.nonNull(position) && !this.level.isClientSide) {
-            if (position.equals(this.lastPos)) {
+        if (!this.level.isClientSide) {
+            if (this.isOnGround()) {
                 this.timeTracker++;
             } else {
-                this.lastPos = position;
                 this.hotpotato$resetTimeTracker();
             }
 
